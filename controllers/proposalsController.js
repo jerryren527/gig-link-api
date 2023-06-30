7a74e6af5d067b80990eba5dded4b67e9be3995d
Jerry Ren <jerryren.5279@gmail.com>
2023-06-30 12:00:15 -0400

    createNewProposal checks for job status Pending, created changeProposalStatus()

--- controllers/proposalsController.js
+++ controllers/proposalsController.js
@@ -1,6 +1,6 @@
 const Proposal = require('../models/Proposal')
 const asyncHandler = require('express-async-handler')
-const {REQUEST_STATUSES, ROLES} = require('../config/constants')
+const {REQUEST_STATUSES, ROLES, JOB_STATUSES, PROPOSAL_STATUSES} = require('../config/constants')
 const User = require('../models/User')
 const Job = require('../models/Job')
 
@@ -22,17 +22,23 @@ const Job = require('../models/Job')
 // @route POST /proposals
 // @access Private
 const createNewProposal = asyncHandler(async (req, res) => {
-  const {jobId, client, freelancer} = req.body
+  const {jobId, freelancer} = req.body
 
-  if (!jobId || !client ||  !freelancer  ) {
-    return res.status(400).json({ message: "JobId, Client, and Freelancer are required."})
+  if (!jobId ||  !freelancer  ) {
+    return res.status(400).json({ message: "JobId, and Freelancer are required."})
+  }
+
+  // Check that Job status is Pending (and not any other status)
+  const job = await Job.findById(jobId).lean().exec()
+  if (job.status !== JOB_STATUSES.Pending) {
+    return res.status(409).json({ message: `Cannot add a proposal to Job (ID ${job._id}) with status ${job.status}. A proposal can only be added to jobs with status Pending.`})
   }
 
   // check for duplicate proposals
-  const duplicate = await Proposal.findOne({ client: client, freelancer: freelancer, jobId: jobId }).lean().exec()
+  const duplicate = await Proposal.findOne({ freelancer: freelancer, jobId: jobId }).lean().exec()
 
   if (duplicate) {
-    return res.status(409).json({ message: "Duplicate proposal."})
+    return res.status(409).json({ message: `Duplicate proposal. Freelancer ${freelancer} has already made a proposal to Job ${jobId}.`})
   }
   
   // create proposal
@@ -55,10 +61,51 @@ const createNewProposal = asyncHandler(async (req, res) => {
     const updatedJobObj = await jobObj.save()
     console.log('updatedJobObj', updatedJobObj)
 
-    return res.status(201).json({ message: `New proposal for job ${jobObj._id} created.`})
+    return res.status(201).json({ message: `New proposal (ID ${proposal._id}) for job ${jobObj._id} created.`})
   } else {
     return res.status(400).json({ message: 'Invalid proposal data received.'})
   }
+})
+
+
+// @desc Change proposal status
+// @route PATCH /proposals/status
+// @access Private
+const changeProposalStatus = asyncHandler(async (req, res) => {
+  const { proposalId, status } = req.body
+
+  // find proposal, change status.
+  const proposal = await Proposal.findById(proposalId).exec()
+
+  if (proposal.status === PROPOSAL_STATUSES.Accepted || proposal.status === PROPOSAL_STATUSES.Declined) {
+    return res.status(409).json({ message: `Proposal ${proposalId} with status ${proposal.status} cannot be changed.`})
+  }
+  const prevStatus =  proposal.status
+  proposal.status = status
+  
+
+  const message = []
+  // If proposal status changed to Accepted, then change Job's status to Accepted, add freelancer property to job, and add freelancer to freelancer's User.activeJobs
+  if (status === PROPOSAL_STATUSES.Accepted) {
+    const job = await Job.findById(proposal.jobId).exec()
+    job.status = JOB_STATUSES.Accepted
+    job.freelancer = proposal.freelancer
+    
+    const result = await User.updateOne({ _id: proposal.freelancer }, { $push: { activeJobs: job }})
+    
+    if (result.modifiedCount === 0) {
+      return res.status(204)
+    }
+
+    message.push(`Changed Job ${job._id} status to ${job.status}. And added Job ${job._id} to freelancer ${proposal.freelancer} activeJobs.`)
+    await job.save()
+  }
+
+  await proposal.save()
+
+  message.push(`Changed Proposal (ID ${proposalId}) from status ${prevStatus} to ${status}.`)
+  return res.status(200).json({ message: message.join(' ')})
+
 })
 
 
@@ -102,5 +149,6 @@ const deleteProposal = asyncHandler(async (req, res) => {
 
 module.exports = {
   createNewProposal,
+  changeProposalStatus,
   deleteProposal
 }
