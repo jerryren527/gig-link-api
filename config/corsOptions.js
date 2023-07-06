8857500e0a902090bc3271a237dcb4a5847f709c
Jerry Ren <jerryren.5279@gmail.com>
2023-07-06 00:27:31 -0400

    added corsOptions

---
+++ config/corsOptions.js
@@ -0,0 +1,18 @@
+const allowedOrigins = [
+  'http://localhost:5173',
+]
+
+const corsOptions = {
+    origin: (origin, callback) => {
+        // Leave in !origin if you want Postman to access it
+        if (allowedOrigins.includes(origin) || !origin) {
+            callback(null, true)
+        } else {
+            callback(new Error('Not allowed by CORS'))
+        }
+    },
+    credentials: true,
+    optionsSuccessStatus: 200
+}
+
+module.exports = corsOptions 
