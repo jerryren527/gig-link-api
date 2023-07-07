94ed3f2521147d8a0b98ebb1ab2fdb5ca3ba4700
Jerry Ren <jerryren.5279@gmail.com>
2023-07-07 19:55:02 -0400

    added /auth route

--- server.js
+++ server.js
@@ -2,14 +2,22 @@ require('dotenv').config()
 const express = require('express')
 const app = express()
 const path = require('path')
-// const cors = require('cors')
+const cors = require('cors')
 const connectDB = require('./config/dbConnect')
 const mongoose  = require('mongoose')
+const cookieParser = require('cookie-parser')
 const PORT = process.env.PORT || 3500
+
+const errorHandler = require('./middlewares/errorHandler')
+const corsOptions = require('./config/corsOptions')
 
 connectDB();
 
+app.use(cors(corsOptions))
+
 app.use(express.json())
+
+app.use(cookieParser())
 
 app.use('/', express.static(path.join(__dirname, 'public')))
 
@@ -20,6 +28,21 @@ app.use('/requests', require('./routes/requestRoutes'))
 app.use('/reviews', require('./routes/reviewRoutes'))
 app.use('/messages', require('./routes/messageRoutes'))
 app.use('/proposals', require('./routes/proposalRoutes'))
+
+app.use('/auth', require('./routes/authRoutes'))
+
+app.all('*', (req, res) => {
+  res.status(404)
+  if (req.accepts('html')) {
+    res.sendFile(path.join(__dirname, 'views', '404.html'))
+  } else if (req.accepts('json')) {
+    res.json({ message: '404 Not Found' })
+  } else {
+    res.type('txt').send('404 Not Found')
+  }
+})
+
+app.use(errorHandler)
 
 mongoose.connection.once('open', () => {
   console.log('Conneced to MongoDB')
