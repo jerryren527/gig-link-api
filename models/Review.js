6670118c9ee84075a7c82bbcc8a185d931dfad53
Jerry Ren <jerryren.5279@gmail.com>
2023-07-11 17:42:11 -0400

    added clientUsername, freelancerUsername

--- models/Review.js
+++ models/Review.js
@@ -6,10 +6,16 @@ const ReviewSchema = new mongoose.Schema({
     ref: 'User',
     required: true
   },
+  clientUsername: {
+    type: String,
+  },
   freelancer: {
     type: mongoose.Schema.Types.ObjectId,
     ref: 'User',
     required: true
+  },
+  freelancerUsername: {
+    type: String,
   },
   review: {
     type: String,
