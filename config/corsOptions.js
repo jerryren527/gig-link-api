3a74d6adf6bb4718b236bf9a5551bbd8211ed0d9
Jerry Ren <jerryren.5279@gmail.com>
2023-07-18 18:51:18 -0400

    added a catch all to send index.html as response

--- config/corsOptions.js
+++ config/corsOptions.js
@@ -1,19 +1,19 @@
 const allowedOrigins = [
-    'https://gig-link.onrender.com',
-    // 'http://localhost:5173',
-]
+	"https://gig-link.onrender.com",
+	// 'http://localhost:5173',
+];
 
 const corsOptions = {
-    origin: (origin, callback) => {
-        // Leave in !origin if you want Postman to access it
-        if (allowedOrigins.includes(origin) || !origin) {
-            callback(null, true)
-        } else {
-            callback(new Error('Not allowed by CORS'))
-        }
-    },
-    credentials: true,
-    optionsSuccessStatus: 200
-}
+	origin: (origin, callback) => {
+		// Leave in !origin if you want Postman to access it
+		if (allowedOrigins.includes(origin) || !origin) {
+			callback(null, true);
+		} else {
+			callback(new Error("Not allowed by CORS"));
+		}
+	},
+	credentials: true,
+	optionsSuccessStatus: 200,
+};
 
-module.exports = corsOptions 
+module.exports = corsOptions;
--- server.js
+++ server.js
@@ -1,55 +1,61 @@
-require('dotenv').config()
-const express = require('express')
-const app = express()
-const path = require('path')
-const cors = require('cors')
-const connectDB = require('./config/dbConnect')
-const mongoose  = require('mongoose')
-const cookieParser = require('cookie-parser')
-const PORT = process.env.PORT || 3500
+require("dotenv").config();
+const express = require("express");
+const app = express();
+const path = require("path");
+const cors = require("cors");
+const connectDB = require("./config/dbConnect");
+const mongoose = require("mongoose");
+const cookieParser = require("cookie-parser");
+const PORT = process.env.PORT || 3500;
 
-const errorHandler = require('./middlewares/errorHandler')
-const corsOptions = require('./config/corsOptions')
+const errorHandler = require("./middlewares/errorHandler");
+const corsOptions = require("./config/corsOptions");
 
 connectDB();
 
-app.use(cors(corsOptions))
+app.use(cors(corsOptions));
 
-app.use(express.json())
+app.use(express.json());
 
-app.use(cookieParser())
+app.use(cookieParser());
 
-app.use('/', express.static(path.join(__dirname, 'public')))
+app.use("/", express.static(path.join(__dirname, "public")));
 
 // Cache static assets for 1 day
-app.use(express.static(path.join(__dirname, 'public'), { maxAge: 86400000 })); // Set maxAge to cache for 1 day
+app.use(express.static(path.join(__dirname, "public"), { maxAge: 86400000 })); // Set maxAge to cache for 1 day
 
+app.use("/", require("./routes/root"));
+app.use("/users", require("./routes/userRoutes"));
+app.use("/jobs", require("./routes/jobRoutes"));
+app.use("/requests", require("./routes/requestRoutes"));
+app.use("/reviews", require("./routes/reviewRoutes"));
+app.use("/messages", require("./routes/messageRoutes"));
+app.use("/proposals", require("./routes/proposalRoutes"));
 
-app.use('/', require('./routes/root'))
-app.use('/users', require('./routes/userRoutes'))
-app.use('/jobs', require('./routes/jobRoutes'))
-app.use('/requests', require('./routes/requestRoutes'))
-app.use('/reviews', require('./routes/reviewRoutes'))
-app.use('/messages', require('./routes/messageRoutes'))
-app.use('/proposals', require('./routes/proposalRoutes'))
+app.use("/auth", require("./routes/authRoutes"));
 
-app.use('/auth', require('./routes/authRoutes'))
+app.get("/*", function (req, res) {
+	res.sendFile(path.join(__dirname, "views", "index.html"), function (err) {
+		if (err) {
+			res.status(500).send(err);
+		}
+	});
+});
 
+app.all("*", (req, res) => {
+	res.status(404);
+	if (req.accepts("html")) {
+		res.sendFile(path.join(__dirname, "views", "404.html"));
+	} else if (req.accepts("json")) {
+		res.json({ message: "404 Not Found" });
+	} else {
+		res.type("txt").send("404 Not Found");
+	}
+});
 
-app.all('*', (req, res) => {
-  res.status(404)
-  if (req.accepts('html')) {
-    res.sendFile(path.join(__dirname, 'views', '404.html'))
-  } else if (req.accepts('json')) {
-    res.json({ message: '404 Not Found' })
-  } else {
-    res.type('txt').send('404 Not Found')
-  }
-})
+app.use(errorHandler);
 
-app.use(errorHandler)
-
-mongoose.connection.once('open', () => {
-  console.log('Conneced to MongoDB')
-  app.listen(PORT, () => console.log(`Server running on port ${PORT}...`))
-})
+mongoose.connection.once("open", () => {
+	console.log("Conneced to MongoDB");
+	app.listen(PORT, () => console.log(`Server running on port ${PORT}...`));
+});
